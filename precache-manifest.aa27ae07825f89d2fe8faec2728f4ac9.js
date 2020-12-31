8270c2291fcbdf68294734cb2cd3218208d6843e
Sunny Sultan <sunnysultan1640@gmail.com>
2020-12-31 18:20:58 +0600

    Updates

---
+++ precache-manifest.aa27ae07825f89d2fe8faec2728f4ac9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0885879c40e796024538c6ff296a377f",
+    "url": "/strativ-quiz/index.html"
+  },
+  {
+    "revision": "517972a7154c4a9adf9e",
+    "url": "/strativ-quiz/static/css/5.6b2ce82f.chunk.css"
+  },
+  {
+    "revision": "e3db4170dbdf80729efa",
+    "url": "/strativ-quiz/static/css/main.dcff707a.chunk.css"
+  },
+  {
+    "revision": "4f9645cd75dfffa8ea3f",
+    "url": "/strativ-quiz/static/js/0.28b310c2.chunk.js"
+  },
+  {
+    "revision": "75a6ac2a2eae8280bd06",
+    "url": "/strativ-quiz/static/js/1.67f108c3.chunk.js"
+  },
+  {
+    "revision": "c21c498e57b1dc880161",
+    "url": "/strativ-quiz/static/js/10.2e38eba9.chunk.js"
+  },
+  {
+    "revision": "1f09f2548a40837c2924",
+    "url": "/strativ-quiz/static/js/11.c8b3b509.chunk.js"
+  },
+  {
+    "revision": "ee077bfcccc7d6ac8491",
+    "url": "/strativ-quiz/static/js/12.6f80f899.chunk.js"
+  },
+  {
+    "revision": "f817984ca519363d706c",
+    "url": "/strativ-quiz/static/js/13.14816009.chunk.js"
+  },
+  {
+    "revision": "40bc725a9e06ff897b68",
+    "url": "/strativ-quiz/static/js/2.fce27133.chunk.js"
+  },
+  {
+    "revision": "517972a7154c4a9adf9e",
+    "url": "/strativ-quiz/static/js/5.4595cc03.chunk.js"
+  },
+  {
+    "revision": "d64f2614eb05ac35b0b0",
+    "url": "/strativ-quiz/static/js/6.f40df2b3.chunk.js"
+  },
+  {
+    "revision": "2d8e16342afbe8dd14b4",
+    "url": "/strativ-quiz/static/js/7.3424db2c.chunk.js"
+  },
+  {
+    "revision": "0b63cfef5952bc1080b8",
+    "url": "/strativ-quiz/static/js/8.e83fcd18.chunk.js"
+  },
+  {
+    "revision": "cc312b5b96333a6634ac",
+    "url": "/strativ-quiz/static/js/9.3b02aab0.chunk.js"
+  },
+  {
+    "revision": "e3db4170dbdf80729efa",
+    "url": "/strativ-quiz/static/js/main.994eb322.chunk.js"
+  },
+  {
+    "revision": "dd9e2fbe8c9ddad5caa6",
+    "url": "/strativ-quiz/static/js/runtime~main.e9d3b1e4.js"
+  }
+]);
